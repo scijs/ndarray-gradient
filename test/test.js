49f9d9518761ea3bb1b62631f78ffa596eda68d7
Mikola Lysenko <mikolalysenko@gmail.com>
2014-10-04 13:08:28 -0500

    add stub for test cases

---
+++ test/test.js
@@ -0,0 +1,11 @@
+'use strict'
+
+var tape = require('tape')
+var pack = require('ndarray-pack')
+var unpack = require('ndarray-unpack')
+var grad = require('../fdg')
+
+tape('ndarray-gradient', function(t) {
+
+  t.end()
+})
